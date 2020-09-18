2af157eecfeafea26d9d8441e68939eef57c0b15
k-shg <s6h1o3go@gmail.com>
2020-09-19 01:29:58 +0900

    [Add] ログイン・ログアウト機能の実装

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,21 @@
+class UserSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to(root_path, notice: 'ログインしました')
+    else
+      flash.now[:alert] = 'ログインに失敗しました'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(root_path, notice: 'ログアウトしました')
+  end
+end
