59ff45c444275aefeca347eeeadd118c4b51f309
k-shg <s6h1o3go@gmail.com>
2020-09-24 01:15:09 +0900

    [Add] 投稿のCRUD機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,51 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
-  def new; end
+  def new
+    @post = current_user.posts.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 
   def create
     @post = current_user.posts.build(post_params)
 
     if @post.save
-      redirect_to root_path
+      redirect_to root_path, success: '投稿しました'
     else
+      flash.now[:danger] = '投稿に失敗しました'
+      render 'new'
+      
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+
+      redirect_to root_path, success: '投稿を更新しました'
+    else
+      flash.now[:danger] = '投稿に失敗しました'
       render 'new'
     end
   end
 
-  def edit; end
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy!
+    redirect_to posts_path
+  end
 
   def post_params
-    params.require(:post).permit(:body, :images)
+    params.require(:post).permit(:body, images: [])
   end
 end
