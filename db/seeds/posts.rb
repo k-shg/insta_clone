520a61609036afb952ae12ec5c5b994c3f04d3e9
k-shg <s6h1o3go@gmail.com>
2020-09-25 14:23:34 +0900

    [Add] ページネーションの実装

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,5 +1,5 @@
 puts 'Start inserting seed "posts" ...'
-User.limit(10).each do |user|
+User.limit(20).each do |user|
   post = user.posts.create(
     body: Faker::Hacker.say_something_smart,
     remote_images_urls: %w[https://picsum.photos/350/350/?random https://picsum.photos/350/350/?random]
