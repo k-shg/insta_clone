aba2f704cb5a94bc598fa6f074091fe0053b2e0b
k-shg <s6h1o3go@gmail.com>
2020-10-02 19:24:25 +0900

    [Add]検索機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,4 +26,6 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 1000 }
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  scope :body_contain, ->(word) { Post.where('body LIKE ?', "%#{word}%") }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :users, only: %i[index new create show]
   resources :posts, shallow: true do
+    collection do
+      get :search
+    end
     resources :comments
   end
   resources :likes, only: %i[create destroy]
