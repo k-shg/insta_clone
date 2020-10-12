0e2fd79fe6a8e938db20ee954b262e1f1582daee
k-shg <s6h1o3go@gmail.com>
2020-10-12 21:37:38 +0900

    [Add] be rails g model chatroom

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_073801) do
+ActiveRecord::Schema.define(version: 2020_10_11_153202) do
 
   create_table "activities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "subject_type"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_10_09_073801) do
     t.datetime "updated_at", null: false
     t.index ["subject_type", "subject_id"], name: "index_activities_on_subject_type_and_subject_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "chatrooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
